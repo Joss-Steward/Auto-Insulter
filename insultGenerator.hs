e2372a833cec276863bb559ee125c99ea1d45b80
Joss Steward <Joss.Steward@gmail.com>
2015-01-21 15:11:22 -0500

    Separated out some of the IO components, cleaned it up a bit.

--- insultGenerator.hs
+++ insultGenerator.hs
@@ -1,20 +1,39 @@
 import System.Random (randomRIO)
+import Control.Monad
 
 pick :: [a] -> IO a
 pick xs = randomRIO (0, length xs - 1) >>= return . (xs !!)
 
-firstPart :: IO [Char]
-firstPart = pick ["Lazy", "Stupid", "Insecure", "Idiotic", "Slimy", "Slutty", "Smelly", "Pompous", "Communist", "Dicknose", "Pie-Eating", "Racist", "Elitist", "Trashy", "Drug-loving", "Butterface", "Tone deaf", "Ugly", "Creepy"]
+firstPart :: [[Char]]
+firstPart = ["lazy", "stupid", "insecure", "idiotic", "slimy", "slutty", "smelly", "pompous", "communist", "dicknose", "pie-eating", "racist", "elitist", "trashy", "drug-loving", "butterface", "tone deaf", "ugly", "creepy"]
 
-secondPart :: IO [Char]
-secondPart = pick ["douche", "ass", "turd", "rectum", "butt", "cock","shit","crotch","bitch","prick","slut","taint","fuck","dick","boner","shart","nut","sphincter"]
+secondPart :: [[Char]]
+secondPart = ["douche", "ass", "turd", "rectum", "butt", "cock","shit","crotch","bitch","prick","slut","taint","fuck","dick","boner","shart","nut","sphincter"]
 
-thirdPart :: IO [Char]
-thirdPart = pick ["pilot","canoe","captain","pirate","hammer","knob","box","jockey","nazi","waffle","goblin","blossum","biscuit","clown","socket","monster","hound","dragon","balloon"]
+thirdPart :: [[Char]]
+thirdPart = ["pilot","canoe","captain","pirate","hammer","knob","box","jockey","nazi","waffle","goblin","blossum","biscuit","clown","socket","monster","hound","dragon","balloon"]
+
+makeInsult :: IO [Char]
+makeInsult = do
+    fp <- pick firstPart
+    sp <- pick secondPart
+    tp <- pick thirdPart
+    return $ fp ++ " " ++ sp ++ " " ++ tp
 
 main = do
-	fp <- firstPart 
-	sp <- secondPart
-	tp <- thirdPart
-	putStrLn $ "You are a " ++ fp ++ " " ++ sp ++ " " ++ tp
-	
+    insult <- makeInsult
+    putStrLn $ "You are a " ++ insult ++ "!"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
